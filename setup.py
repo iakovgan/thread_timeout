524c9507af6c345e25afe0d70cbe0751c2296ccf
Iakov Gan <iakovgan+github@gmail.com>
2015-09-07 23:17:08 +0200

    get version from module

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
 
+import re
+# hacky way to get __version__ from __init__
+# we can not just import since some dependancies could be not installed yet 
+metadata = dict(re.findall("\n__([a-z]+)__ = '([^']+)'", open('hostsync/__init__.py').read()))
+__version__ = metadata['version'] 
+
 setup(
     name='thread_timeout',
-    version='1.0',
+    version=__version__,
     description='''Decorator to execute functionin in separate thread with timeout''',
     author='George Shuklin',
     author_email='george.shuklin@gmail.com',
