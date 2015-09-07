53fc67b8a993945d6a3324d053f8b378e971abe8
Iakov Gan <iakovgan+github@gmail.com>
2015-09-07 23:37:45 +0200

    rename to rst (accpeted by pypi)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='thread_timeout',
     version=__version__,
     description='''Decorator to execute functionin in separate thread with timeout''',
-    long_description=read("README.md"),
+    long_description=read("README.rst"),
     author='George Shuklin',
     author_email='george.shuklin@gmail.com',
     url='https://github.com/amarao/thread_timeout',
