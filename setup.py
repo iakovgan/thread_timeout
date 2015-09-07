3d9b195e14d1cc034198b6270758efa25e926716
Iakov Gan <iakovgan+github@gmail.com>
2015-09-07 23:19:45 +0200

    add long_description

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ import re
 metadata = dict(re.findall("\n__([a-z]+)__ = '([^']+)'", open('thread_timeout/__init__.py').read()))
 __version__ = metadata['version'] 
 
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='thread_timeout',
     version=__version__,
     description='''Decorator to execute functionin in separate thread with timeout''',
+    long_description=read("README.md"),
     author='George Shuklin',
     author_email='george.shuklin@gmail.com',
     url='https://github.com/amarao/thread_timeout',
